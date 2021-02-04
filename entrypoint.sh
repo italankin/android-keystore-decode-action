a363bc39d44b6b4e2feabfe95fea861ff7048d37
Igor Talankin <italankin@gmail.com>
2021-02-04 20:45:17 +0500

    Fix keystore location

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,14 +2,15 @@
 
 mkdir -p $INPUT_OUTPUT_DIR
 SECRET_PLAIN="$INPUT_OUTPUT_DIR/.secret_plain"
+KEYSTORE_OUTPUT="$INPUT_OUTPUT_DIR/$INPUT_KEYSTORE_NAME"
 
 echo "$INPUT_ENCRYPTED_KEYSTORE" | base64 -d > $SECRET_PLAIN || exit 1
 gpg --quiet --batch --yes --decrypt --passphrase="$INPUT_DECRYPT_PASSWORD" \
-    --output "$INPUT_OUTPUT_DIR/$INPUT_KEYSTORE_NAME" $SECRET_PLAIN || exit 1
+    --output $KEYSTORE_OUTPUT $SECRET_PLAIN || exit 1
 rm $SECRET_PLAIN
 
 PROPS="$INPUT_OUTPUT_DIR/$INPUT_PROPERTIES_NAME"
-echo "keystore=keystore-release/release.keystore" > $PROPS
+echo "keystore=$KEYSTORE_OUTPUT" > $PROPS
 echo "keystore_password=$INPUT_KEYSTORE_PASSWORD" >> $PROPS
 echo "alias=$INPUT_KEY_ALIAS" >> $PROPS
 echo "alias_password=$INPUT_KEY_ALIAS_PASSWORD" >> $PROPS
